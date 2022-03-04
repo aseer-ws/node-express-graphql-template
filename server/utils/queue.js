ee9561bffd6fc15d4f14976305809d86162454c2
Aseer KT <aseer@wednesday.is>
2022-03-04 14:48:33 +0530

    feat: Run a CRON job at 12 AM everyday

--- server/utils/queue.js
+++ server/utils/queue.js
@@ -1,15 +1,27 @@
 import Bull from 'bull';
 import moment from 'moment';
 
+/** @type {Object.<string, Bull.Queue>} */
 const queues = {};
 
 export const QUEUE_NAMES = {
-  SCHEDULE_JOB: 'scheduleJob'
+  SCHEDULE_JOB: 'scheduleJob',
+  MIDNIGHT_CRON: 'midnightCron'
+};
+
+const CRON_EXPRESSIONS = {
+  MIDNIGHT: '0 0 * * *'
 };
 
 export const QUEUE_PROCESSORS = {
   [QUEUE_NAMES.SCHEDULE_JOB]: (job, done) => {
     console.log(`${moment()}::Job with id: ${job.id} is being executed.\n`);
+    done();
+  },
+  [QUEUE_NAMES.MIDNIGHT_CRON]: (job, done) => {
+    console.log({ job, done });
+    console.log(`${moment()}:: The "${QUEUE_NAMES.MIDNIGHT_CRON}" is being executed at 12:00 AM`);
+    done();
   }
 };
 
@@ -17,7 +29,9 @@ export const initQues = () => {
   console.log('init queues');
   Object.keys(QUEUE_PROCESSORS).forEach(queueName => {
     queues[queueName] = getQueue(queueName);
+    queues[queueName].process(QUEUE_PROCESSORS[queueName]);
   });
+  queues[QUEUE_NAMES.MIDNIGHT_CRON].add({}, { repeat: { cron: CRON_EXPRESSIONS.MIDNIGHT } });
 };
 
 /** @returns {Bull.Queue} */
